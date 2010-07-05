3cf5d9979a5b99e94361dc6ebd1ac108b25fc007
dsc <david.schoonover@gmail.com>
2010-07-05 01:12:54 -0400

    Fixes homepage URL.

--- setup.py
+++ setup.py
@@ -3,21 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name = "coffeecup",
-    version = "0.0.1",
+    version = "0.1.0",
     description = "CoffeScript WSGI Middleware",
     long_description = """
         CoffeeCup is a WSGI middleware wrapper to transparently compile CoffeeScript files (things ending in `.coffee`) into javascript.
     """,
-    url = "http://tire.less.ly/hacking/coffeecup",
+    url = "http://github.com/dsc/coffeecup",
     
     author = "David Schoonover",
     author_email = "dsc@less.ly",
     
     py_modules = ['coffeecup'],
-    # packages = ['coffeecup'],
-    install_requires=[
-        # "demjson>=1.4",
-    ],
     keywords = [],
     classifiers = [],
     zip_safe = True,
